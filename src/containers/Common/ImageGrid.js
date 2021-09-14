ddbc74dbd69a7185f203d2fffe6d38c10e31d7d4
sameer <sameer@3bigs.com>
2021-09-14 04:22:26 +0000

    image grid added

---
+++ src/containers/Common/ImageGrid.js
@@ -0,0 +1,94 @@
+import React, { useState,useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+
+
+export default function ImageGrid({data}) {
+  const [displaySamples, setDisplaySamples] = useState(false)
+  const [showModal, setShowModal] = React.useState(false);
+  const [selectedImageId, setSelectedImageId] = useState('')
+
+
+  function modalShow(e){
+    setShowModal(true)
+    setSelectedImageId(e.target.id)
+  }
+
+  useEffect(()=>{
+    console.log(data);
+  },[data])
+
+
+  return (
+    <>
+      <div class="container grid grid-cols-3 gap-2 mx-auto">
+        <div class="w-full rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                id="1" alt="image" onClick={modalShow}/>
+        </div>
+        <div class="w-full rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                id="2" alt="image" onClick={modalShow}/>
+        </div>
+        <div class="w-full rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                id="3" alt="image" onClick={modalShow}/>
+        </div>
+        <div class="w-full rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                id="4" alt="image" onClick={modalShow}/>
+        </div>
+        <div class="w-full rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                id="5" alt="image" onClick={modalShow}/>
+        </div>
+        <div class="w-full rounded">
+            <img src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+                id="6" alt="image" onClick={modalShow}/>
+        </div>
+      </div>
+    {showModal ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Modal Title
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  {selectedImageId}
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  )
+
+}
