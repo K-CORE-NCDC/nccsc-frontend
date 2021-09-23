297cea69784df9e3dbc430cb2813f072de8a8101
sameer <sameer@3bigs.com>
2021-09-23 05:22:04 +0000

    changes made in login and signup

--- src/containers/Login/login.js
+++ src/containers/Login/login.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios'
 import config from '../../config'
+import {
+  UserIcon
+} from '@heroicons/react/outline'
 
 const LoginComponent = () => {
     const [userFormData, setUserFormData] = useState({ username: "", password: "" })
@@ -46,26 +49,51 @@ const LoginComponent = () => {
 
     return (
         <div>
-            <section className="mt-10 flex flex-col">
-                <div className="flex flex-1 items-center justify-center">
-                    <div className="rounded-lg sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center">
-                        <form onSubmit={formSubmitAction} className="text-center">
-                            <h1 className="font-bold tracking-wider text-3xl mb-8 w-full text-gray-600">
-                                Sign in
-                            </h1>
-                            <div className="py-2 text-left">
-                                <input value={userFormData.username} onChange={updateUserNamePassword} type="text" name="username" className={`bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 ${errorClass} `} placeholder="username" />
-                            </div>
-                            <div className="py-2 text-left">
-                                <input value={userFormData.password} onChange={updateUserNamePassword} name="password" type="password" className={`bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 ${errorClass} `} placeholder="Password" />
-                                {errorMessage}
-                            </div>
-                            <div className="py-2">
-                                <button type="submit" className="border-2 border-gray-100 focus:outline-none bg-purple-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg focus:border-gray-700 hover:bg-purple-700">
-                                    Sign In
-                                </button>
-                            </div>
-                        </form>
+            <section className="mt-10 flex flex-col items-center justify-center">
+                <div>
+                    <span className="text-7xl font-bold text-gray-800">Login</span>
+                </div>
+                <div className="my-14">
+                    <h1 className="font-bold text-3xl text-gray-800">Please use the service after logging in.</h1>
+                </div>
+                <div className="my-32">
+                    <div className="border-b-2 border-gray-600 pt-8 pb-8">
+                      <h1 className="font-bold text-3xl text-red-500"> If an error in consecutive password input (5 times) occurs, the account is locked.</h1>
+                    </div>
+                    <div className="grid grid-cols-3 border-b-2 border-gray-600 pt-12 pb-12">
+                        <div className="pt-6 pl-48 col-span-1">
+                          <h1 className="font-bold">login ID_</h1>
+                        </div>
+                        <div>
+                        <div className="mb-4 pr-45 col-span-2">
+                            <input className="shadow appearance-none border rounded-lg w-full py-8 px-5 text-gray-700 leading-tight focus:border-blue-500  w-28" id="username" type="text" placeholder="Please Enter your id"/>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-3 border-b-2 border-gray-600 pt-12 pb-12">
+                        <div className="pt-6 pl-48 col-span-1">
+                          <h1 className="font-bold">Password _</h1>
+                        </div>
+                        <div>
+                        <div className="mb-4 pr-45 col-span-2">
+                            <input className="shadow appearance-none border rounded-lg w-full py-8 px-5 text-gray-700 leading-tight focus:border-blue-500  w-28" id="password" type="text" placeholder="Please Enter a password "/>
+                        </div>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-3 pt-12">
+                        <div className="w-full col-span-3">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white h-28 font-bold py-2 px-4 border border-blue-700 w-full rounded" style={{backgroundColor:"#194872"}}>
+                              <UserIcon className="h-14 w-12 inline text-main-white"/> <span>Login</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-3 pt-12">
+                        <div className="w-full col-span-3">
+                          <div className="float-right">
+                            <a href="#" className="border-r-2 border-gray-600 pr-5">Find ID</a>
+                            <a href="#" className="pl-5">Find password</a>
+                          </div>
+                        </div>
                     </div>
                 </div>
             </section>
---
+++ src/containers/Signup/index.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import config from '../../config'
+import MemberShip from './MemberShip'
+import TermsOfUse from './TermsOfUse'
+import {
+  UserIcon
+} from '@heroicons/react/outline'
+
+const SignupComponent = () => {
+    const [errorClass, setErrorClass] = useState("")
+    const [errorMessage, setErrorMessage] = useState([])
+    const [nextstep, setNextStep] = useState(1)
+
+    const updateStep_ = (star) => {
+      let step = nextstep
+      setNextStep( step + 1)
+    }
+
+    useEffect(()=>{
+
+    },[nextstep])
+
+    const components = (nextstep) => {
+      switch (nextstep) {
+          case 1:
+            return (
+              <TermsOfUse updateStep={updateStep_}/>
+            )
+          case 2:
+            return (
+              <MemberShip />
+            )
+          default:
+             // do nothing
+          }
+    }
+
+
+    return (
+        <div>
+            <div className="flex flex-row justify-center items-center gap-3 mt-6">
+              <div className={"p-6 text-5xl font-semibold "+(nextstep === 1?"border-b-2 border-blue-800":"border-b-2 border-gray-300")}>
+              Comments
+              </div>
+              <div className={"p-6 text-5xl font-semibold "+(nextstep === 2?"border-b-2 border-blue-800":"border-b-2 border-gray-300")}>Join membership</div>
+            </div>
+            <div className="mt-5">
+            {components(nextstep)}
+            </div>
+        </div>
+    );
+}
+
+export default SignupComponent;
