04aea297880c9989d3afa5f3a62133e96d18a0d8
sameer-7376 <sameer@3bigs.com>
2024-01-05 09:30:42 +0000

    sohel:latest changes

--- src/containers/Common/Onco.js
+++ src/containers/Common/Onco.js
@@ -13,6 +13,9 @@ const OncoCmp = React.forwardRef(
     const [customName, setCustomName] = useState({});
     const [oncoprintObj, setOncoprintObj] = useState({});
     const [clickType, setClickType] = useState([]);
+    const [state, setState] = useState({});
+    const [name, setName] = useState('');
+    const BrstKeys = useSelector((data) => data.dataVisualizationReducer.Keys);
     let names_variant = {
       'Missense Mutation': 'variant_classification||Missense_Mutation||4',
       'Nonsense Mutation': 'variant_classification||Nonsense_Mutation||6',
@@ -21,7 +24,7 @@ const OncoCmp = React.forwardRef(
       'In Frame Del': 'variant_classification||In_Frame_Del||6',
       'Frame Shift Ins': 'variant_classification||Frame_Shift_Ins||6',
       'Frame Shift Del': 'variant_classification||Frame_Shift_Del||6',
-      Germline: 'variant_classification||Missense_Mutation||6',
+      'Germline': 'variant_classification||Missense_Mutation||6',
       'Protein Downregulation (value <= 0.5)': 'protein||down||4',
       'Protein Upregulation (value >= 1.5)': 'protein||up||4',
       'mRNA Downregulation (z-score <= -1)': 'regulation||down||3',
@@ -31,12 +34,8 @@ const OncoCmp = React.forwardRef(
       'Cnv (value >= 3)': 'cnv||red'
     };
 
-    const [state, setState] = useState({});
-    const BrstKeys = useSelector((data) => data.dataVisualizationReducer.Keys);
-
     const drawChart = (w, gData, cData, rule_types, inputRule) => {
       var oncoprint;
-
       if ($('#oncoprint-glyphmap').length > 0) {
         $('#oncoprint-glyphmap').empty();
       }
@@ -155,6 +154,7 @@ const OncoCmp = React.forwardRef(
           });
         }
       }
+
       var clinicalData = {};
       if (cData) {
         clinicalData = cData;
@@ -167,8 +167,8 @@ const OncoCmp = React.forwardRef(
       var global_mut_datum = clinicalData['globalMutCnt'];
       var mut_datum = clinicalData['mutCnt'];
       var custom_datum = clinicalData['custom'];
-      
-      
+
+
 
       if (custom_datum.length > 0) {
         for (let i = 0; i < custom_datum.length; i++) {
@@ -177,7 +177,7 @@ const OncoCmp = React.forwardRef(
             customName[originDatum.displayName];
           clinical_custom_track_params['label'] = customName[originDatum.displayName];
           clinical_custom_track_params['description'] = customName[originDatum.displayName];
-          console.log(clinical_custom_track_params)
+          // console.log(clinical_custom_track_params)
           var track_id = oncoprint.addTracks([_.clone(clinical_custom_track_params)])[0];
           oncoprint.setTrackInfo(track_id, '');
           // console.log(custom_datum[i]['data'])
@@ -267,18 +267,20 @@ const OncoCmp = React.forwardRef(
       setTimeout(function () {
         if (rule_types.length > 0) {
           var legends = document.getElementsByClassName('legends');
-          for (let r = 0; r < rule_types.length; r++) {
-            for (let l = 0; l < legends.length; l++) {
-              if (
-                legends[l].textContent in names_variant &&
-                names_variant[legends[l].textContent] === rule_types[r]
-              ) {
-                legends[l].classList.add('linethrough');
+          for (let l = 0; l < legends.length; l++) {
+            const legendText = legends[l]?.textContent;
+            if (legendText in names_variant) {
+              const ruleType = names_variant[legendText];
+              if (rule_types.includes(ruleType.trim())) {
+                legends[l].style.opacity = 0.5; // Set the opacity value (0.5)
+              } else {
+                legends[l].style.opacity = '';
               }
             }
           }
+
         }
-      }, 100);
+      }, 50);
       setOncoprintObj(oncoprint);
     };
 
@@ -301,7 +303,7 @@ const OncoCmp = React.forwardRef(
     useEffect(() => {
       if (data) {
         let custom_name = {};
-        
+
         if (customFilterJson && project_id) {
           for (let cn = 0; cn < customFilterJson.length; cn++) {
             custom_name[customFilterJson[cn]['id']] = customFilterJson[cn]['name'];
@@ -319,43 +321,6 @@ const OncoCmp = React.forwardRef(
       }
     }, [data]);
 
-    document.addEventListener('click', function (e) {
-      let elem = e.target.parentNode;
-      if (elem) {
-        if (elem.classList.contains('legends')) {
-          let name = elem.getAttribute('data-text');
-
-          if (name in names_variant) {
-            let types = clickType ? clickType : [];
-            let zx = names_variant[name];
-            let index = types.indexOf(zx);
-            let r = zx.split('||');
-            let key = r[0];
-            let value = r[1];
-            let z = r[2];
-            let rule = window.geneticrules.genetic_rule_set_custom;
-            if (index > -1) {
-              types.splice(index, 1);
-              rule.rule_params[key][value]['shapes'][0]['z'] = z;
-            } else {
-              types.push(zx);
-              rule.rule_params[key][value]['shapes'][0]['z'] = 0;
-            }
-            setClickType(types);
-            let gData = state['geneData'];
-            let cData = state['clinicalData'];
-            if (gData) {
-              if (gData.length > 0) {
-                drawChart(width - 300, gData, cData, clickType, rule);
-              }
-            }
-          }
-          e.preventDefault();
-          e.stopPropagation();
-        }
-      }
-    });
-
     useEffect(() => {
       if (Object.keys(state).length > 0) {
         let gData = state['geneData'];
@@ -363,6 +328,67 @@ const OncoCmp = React.forwardRef(
         drawChart(width - 300, gData, cData, clickType, inputRule);
       }
     }, [state, inputRule]);
+
+    document.addEventListener('click', (e) => {
+      const elem = e.target.closest('.legends');
+
+      if (elem) {
+        if (!elem.hasAttribute('clicked')) {
+          elem.setAttribute('clicked', '');
+          const name = elem.getAttribute('data-text');
+          if (name in names_variant) {
+            setClickType((prevClickType) => {
+              // Check if the name already exists:
+              const nameExists = prevClickType.includes(names_variant[name]);
+
+              // Construct the new array based on existence:
+              return nameExists
+                ? prevClickType.filter((existingName) => existingName !== names_variant[name]) // Remove if it exists
+                : [...prevClickType, names_variant[name]]; // Add if it doesn't exist
+            });
+            setName(name);
+          }
+        }
+      }
+
+    });
+
+    // Reset the "clicked" attribute for future clicks:
+    document.querySelectorAll('.legends').forEach((elem) => {
+      elem.addEventListener('click', () => {
+        elem.removeAttribute('clicked');
+      });
+    });
+
+    useEffect(() => {
+      if (name) {
+
+        const { genetic_rule_set_custom: originalRule } = window.geneticrules;
+        const rule = JSON.parse(JSON.stringify(originalRule)); // Create a deep copy of the rule
+
+        const { geneData, clinicalData } = state;
+
+        if (clickType?.length > 0) {
+          for (const r of clickType) {
+            let zx = '';
+            for (const [_, val] of Object.entries(names_variant)) {
+              if (val === r) {
+                zx = val;
+                break;
+              }
+            }
+            const [paramKey, paramValue] = zx.split('||');
+            rule.rule_params[paramKey][paramValue]['shapes'][0]['z'] = 0;
+          }
+        }
+
+        if (geneData && geneData.length > 0) {
+          drawChart(width - 300, geneData, clinicalData, clickType, rule);
+        }
+      }
+    }, [clickType])
+
+
 
     const makezoom = (e, name) => {
       $('.' + name).click();
